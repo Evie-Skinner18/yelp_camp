692688b43d5993edf68a8bd789f3ff9cb311fcee
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-10-28 18:41:01 +0000

    Made start of the app wth app.js and two routes. Now a user can go to the landing page, navigate to the Campgrounds page and view all campgrounds that are currently stored in an array inside app.js

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ app.get('/', (req, res)=> {
 // allow user to READ all camp grounds. This is an array for now but will be rows in a MongoDB later
 app.get('/campgrounds', (req, res)=> {
     let campgrounds = [
-        {name: 'Salmon Creek', image: ''},
-        {name: 'Rodborough Fort', image: ''},
-        {name: 'WOMAD Festival', image: ''}
+        {name: 'Salmon Creek', image: 'https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=649&q=80'},
+        {name: 'Rodborough Fort', image: 'https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'},
+        {name: 'WOMAD Festival', image: 'https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80'}
     ];
 
-
+    res.render('campgrounds', {campgrounds: campgrounds});
 });
 
 
