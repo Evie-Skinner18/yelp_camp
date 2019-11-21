6841480ce01f884f5300b45e6aa8c5a4b0d432d8
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-11-21 16:34:11 +0000

    Now have a working app that communicates with a DB and can do the C and R of CRUD operations on it

--- app.js
+++ app.js
@@ -7,16 +7,6 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-// mini DB inside an array
-// let campgrounds = [
-//     {name: 'Salmon Creek', image: 'https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=649&q=80'},
-//     {name: 'Rodborough Fort', image: 'https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'},
-//     {name: 'WOMAD Festival', image: 'https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80'},
-//     {name: 'WOMAD Festival', image: 'https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80'},
-//     {name: 'WOMAD Festival', image: 'https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80'},
-//     {name: 'WOMAD Festival', image: 'https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80'},
-//     {name: 'WOMAD Festival', image: 'https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80'}
-// ];
 
 // connect to the DB
 mongoose.connect('mongodb://localhost/yelpCamp', {useNewUrlParser: true, useUnifiedTopology: true});
@@ -49,8 +39,14 @@ app.get('/', (req, res)=> {
 
 // allow user to READ all camp grounds. This is an array for now but will be rows in a MongoDB later
 app.get('/campgrounds', (req, res)=> {
-
-    res.render('campgrounds', {campgrounds: campgrounds});
+    Campground.find(function(err, allCampgrounds){
+        if(err){
+            res.send('Oh no I can\'t find any campgrounds!');
+        }
+        else{
+            res.render('campgrounds', {campgrounds: allCampgrounds});
+        }
+    });
 });
 
 // route to the form page. User will CREATE a new camp ground via the form and submit this. Submit btn will use that POST route below
@@ -61,12 +57,19 @@ app.get('/campgrounds/new', (req, res)=> {
 // allow user to CREATE a new campground. Following the RESTful convention, both the POST and GET methods for the campground resource
 // should point to /campgrounds
 app.post('/campgrounds', (req, res)=> {
-    // grab data from the user's form and add it into the campgrounds array
+    // grab data from the user's form
     let campgroundName = req.body.name;
     let campgroundImage = req.body.image;
     let newCampground = {name: campgroundName, image: campgroundImage};
-    campgrounds.push(newCampground);
-    res.redirect('/campgrounds');
+    // create a new campground an d save to the DB
+    Campground.create(newCampground, function(err, newlyCreatedCampground){
+        if(err){
+            res.send('Oh no there is an error!');
+        }
+        else{
+            res.redirect('/campgrounds');
+        }
+    });
 })
 
 
