fe0ee637621a055411e16774891938075c75feda
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-11-21 17:07:18 +0000

    Added some notes about RESTful convention

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 
 
 // connect to the DB
-mongoose.connect('mongodb://localhost/yelpCamp', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/yelpCamp', {useNewUrlParser: true, useUnifiedTopology: true});
 
 // set up schema
 const campgroundSchema = new mongoose.Schema({
@@ -20,24 +20,14 @@ const campgroundSchema = new mongoose.Schema({
 // model the campground in JS using Mongoose
 const Campground = mongoose.model('Campground', campgroundSchema);
 
-// Campground.create({
-//     name: 'Rodborough Fort',
-//     image: 'https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-// }, function(err, campground){
-//     if(err){
-//         console.log('Oh no there\'s been an error!');
-//     }
-//     else{
-//         console.log(`The ${campground.name} campground has been added to the DB`);
-//     }
-// });
 
 // root route
 app.get('/', (req, res)=> {
     res.render('landing');
 });
 
-// allow user to READ all camp grounds. This is an array for now but will be rows in a MongoDB later
+// INDEX - show all campgrounds
+// allow user to READ all camp grounds. This shows records in the campgrounds collection of the yelpCamp DB
 app.get('/campgrounds', (req, res)=> {
     Campground.find(function(err, allCampgrounds){
         if(err){
@@ -49,13 +39,15 @@ app.get('/campgrounds', (req, res)=> {
     });
 });
 
-// route to the form page. User will CREATE a new camp ground via the form and submit this. Submit btn will use that POST route below
+// NEW
+// route to the create form page shown to the user. Submit btn will use that POST route below
 app.get('/campgrounds/new', (req, res)=> {
     res.render('new');
 })
 
-// allow user to CREATE a new campground. Following the RESTful convention, both the POST and GET methods for the campground resource
-// should point to /campgrounds
+// CREATE
+// allow user to create a new campground and POST it. Following the RESTful convention, both the POST and GET methods 
+//for the campground resource should point to /campgrounds
 app.post('/campgrounds', (req, res)=> {
     // grab data from the user's form
     let campgroundName = req.body.name;
@@ -78,3 +70,14 @@ const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Yelp Camp has started!');
 });
+
+
+
+/* RESTFUL ROUTES for the campground resource
+
+Name        Url             Http verb   Desc  
+========================================================================
+INDEX       /campgrounds    GET         Display list of all campgrounds
+NEW        /campgrounds/new GET         Display form to create new campground
+CREATE     /campgrounds     POST        Add newly created campground to DB 
+SHOW       /campgrounds/:id GET         Show info about this particular campground 
